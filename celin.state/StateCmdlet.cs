916c450793dd5ddb2ffc0a8fe7b2ec665731c528
Finnur Bragason <fbragason@outlook.com>
2023-11-07 10:48:37 +0800

    .

--- celin.state/StateCmdlet.cs
+++ celin.state/StateCmdlet.cs
@@ -47,7 +47,7 @@ public class Use : PSCmdlet
 	}
 
 	[Cmdlet(VerbsCommon.Set, Nouns.Base)]
-	[Alias("cset", "cs")]
+	[Alias("cset")]
 	public class Set : BaseCmdlet
 	{
 		[Parameter(Position = 0, Mandatory = true)]
